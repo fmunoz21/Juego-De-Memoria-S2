49c2873e0f1550b6a6b34cd0ac9f314fe0991ca5
Frederick Munoz <frederickmunoz@fredericks-macbook-pro.local>
2018-01-06 18:37:41 -0600

    Initial Commit

---
+++ Juego de Memoria/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Juego de Memoria
+//
+//  Created by Frederick Munoz on 1/6/18.
+//  Copyright © 2018 Frederick Munoz. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
